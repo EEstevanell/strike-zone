8212b60eb8726d1b24929791ffe6a4bbe20c3746
Ernesto Estevanell <ernestoestevanellv@gmail.com>
2023-03-18 17:40:42 -0400

    fixed ransc for parabola. added support for exporting trajectory image

--- src/trajectory.py
+++ src/trajectory.py
@@ -35,6 +35,7 @@ class ImageProcessor:
     @staticmethod
     def video_back_substraction(frames, medianFrame):
         grayMedianFrame = cv2.cvtColor(medianFrame, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow('frame2', grayMedianFrame)
 
         result_frames = []
         for frame in frames:
@@ -141,24 +142,23 @@ class BlobDetector:
         return [(*[list(t) for t in zip(*coordinates)], frame) for coordinates, frame in items]
 
 
-class BlobFilter:
-    pass
-
-
 class Trajectory:
     def __init__(self, unpacked_video_blobs) -> None:
+        self.data = unpacked_video_blobs
         self.x_distribution = [(x, frame)
                                for xs, _, frame in unpacked_video_blobs for x in xs]
         self.y_distribution = [(y, frame)
                                for _, ys, frame in unpacked_video_blobs for y in ys]
+        self.x_regressor = None
+        self.y_regressor = None
+        self.detected_points = None
 
-    def plot_x_distribution(self):
+    def plot_x_distribution(self, predictor = None ,pids = None):
         import matplotlib.pyplot as plt
 
-        y = [x[0] for x in self.x_distribution]
-        x = [x[1] for x in self.x_distribution]
-
-        plt.scatter(x, y)
+        source = self.x_distribution if pids is None else np.array(self.x_distribution)[pids]
+        y = [x[0] for x in source]
+        X = [x[1] for x in source]
 
         # Adding the title
         plt.title("X-Distribución")
@@ -167,15 +167,24 @@ class Trajectory:
         plt.ylabel("coordenada x del blob")
         plt.xlabel("Número de cuadro")
 
+        if predictor is not None:
+            plt.style.use("seaborn-darkgrid")
+
+            min_X = min(X)
+            max_X = max(X)
+
+            x_axis = np.linspace(min_X, max_X, int(max_X - min_X))
+            plt.plot(x_axis, predictor.predict(x_axis), c="peru")
+        
+        plt.scatter(X, y)
         plt.show()
 
-    def plot_y_distribution(self):
+    def plot_y_distribution(self, predictor = None, pids = None):
         import matplotlib.pyplot as plt
 
-        y = [y[0] for y in self.y_distribution]
-        x = [y[1] for y in self.y_distribution]
-
-        plt.scatter(x, y)
+        source = self.y_distribution if pids is None else np.array(self.y_distribution)[pids]
+        y = [y[0] for y in source]
+        X = [y[1] for y in source]
 
         # Adding the title
         plt.title("Y-Distribución")
@@ -184,10 +193,20 @@ class Trajectory:
         plt.ylabel("coordenada y del blob")
         plt.xlabel("Número de cuadro")
 
+        if predictor is not None:
+            plt.style.use("seaborn-darkgrid")
+
+            min_X = min(X)
+            max_X = max(X)
+
+            x_axis = np.linspace(min_X, max_X, int(max_X - min_X))
+            plt.plot(x_axis, predictor.predict(x_axis), c="peru")
+            
+        plt.scatter(X, y)
         plt.show()
 
-    def detect_line(self, frame, x):
-        line_ransac = RANSAC(
+    def _detect_line(self, plot=False):
+        regressor = RANSAC(
             n=2,
             t=1,
             d=2,
@@ -196,51 +215,154 @@ class Trajectory:
             loss=min_distance_loss,
             metric=count_out_of_geo_error
         )
-        pass
 
-    def detect_parab(self, frame, y):
-        pass
+        y = [x[0] for x in self.x_distribution] # frames
+        X = [x[1] for x in self.x_distribution] # x position from x distribution
 
-    def detect_trajectory(self, frame, x, y):
-        pass
+        point_to_pos = dict()
+        for i in range(len(y)):
+            point_to_pos[(X[i], y[i])] = i
 
+        regressor.fit(X, y, sequential=True)
+
+        if plot:
+            import matplotlib.pyplot as plt
+            
+            plt.title(f"{regressor.best_fit} Matches: {len(X) - regressor.best_error}")
+
+            min_X = min(X)
+            max_X = max(X)
+
+            line = np.linspace(min_X, max_X, int(max_X - min_X))
+            plt.plot(line, regressor.predict(line), c="peru")
+            plt.scatter(X, y)
+
+            plt.show()
+
+        return regressor, [point_to_pos[(regressor.inlier_X[i], regressor.inlier_y[i])] for i in range(len(regressor.inlier_X))] if regressor.inlier_X is not None else None
+
+    def _detect_parab(self, pids, plot=False):
+        regressor = RANSAC(
+            n=3, # amount of points to train the parabola
+            t=1, # distance threshold
+            d=3, # minimum amount of matching points to the parabola
+            k=500, # max amount of iterations (retries) if not sequential training
+            acc_error=2,
+            model=Parabola(), 
+            loss=min_distance_loss, 
+            metric=count_out_of_geo_error
+        )
+
+        # only get fitted points by previous line
+        source = np.array(self.y_distribution)
+        y = [x[0] for x in source[pids]] # frames
+        X = [x[1] for x in source[pids]] # y position from y distribution
+    
+
+        point_to_pos = dict()
+        for i in range(len(y)):
+            point_to_pos[(X[i], y[i])] = i
+
+        regressor.fit(X, y, sequential=True)
+
+        if plot:
+            import matplotlib.pyplot as plt
+
+            plt.title(f"{regressor.best_fit} Matches: {len(X) - regressor.best_error}")
+            plt.style.use("seaborn-darkgrid")
+
+            min_X = min(X)
+            max_X = max(X)
+
+            line = np.linspace(min_X, max_X, int(max_X - min_X))
+            plt.plot(line, regressor.predict(line), c="peru")
+            plt.scatter(X, y)
+        
+            plt.show()
+        return regressor, [point_to_pos[(regressor.inlier_X[i], regressor.inlier_y[i])] for i in range(len(regressor.inlier_X))] if regressor.inlier_X is not None else None
+    
+    def detect_trajectory(self, tries=10):
+        for _ in range(tries):
+            line, pids = self._detect_line()
+            parab, p_pids = self._detect_parab(pids)
+            if (parab is not None):
+                self.detected_points = np.array(pids)[p_pids]
+                self.x_regressor, self.y_regressor = (line, parab)
+        return self
+
+    def get_first_ocurrence_frame(self):
+        if (self.x_distribution is None):
+            raise Exception("No data provided")
+            
+        if (self.detected_points is None):
+            raise Exception("No trajectory detected")
+        
+        return self.x_distribution[self.detected_points[0]][1]
+
+    def predict(self, frame) -> Tuple[int, int]:
+        px = self.x_regressor.predict([frame])[0]
+        py = self.y_regressor.predict([frame])[0]
+        return px, py
 
 class VideoTrajectory:
-    pass
+    def __init__(self, trajectory=None) -> None:
+        self.trajectory = trajectory
+
+    def compute_trajectory(self, video_path):
+        cap, frames = load_video(video_path)
+
+        self.capture = cap
+        self.frames = frames
+
+        # Obtain frame size information using get() method
+        self.frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.frame_size = (self.frame_width, self.frame_height)
+        self.fps = 20
+
+        medianFrame = ImageProcessor.get_median_frame(cap)
+        cap.release()
+
+        fg_video = ImageProcessor.video_back_substraction(frames, medianFrame)
+        result = BlobDetector.video_detector_from_frames(fg_video)
+        video_blobs = BlobDetector.unpack_video_blobs(result)
+
+        trajectory = Trajectory(video_blobs)
+        self.trajectory = trajectory.detect_trajectory(tries=2)
+
+        # # write the output
+        # output = cv2.VideoWriter(r'D:\Work\UH-IA\Computer Vision\o.avi', cv2.VideoWriter_fourcc('M','J','P','G'), 20, frame_size)
+        # for frame in frames:
+        #     output.write(frame)
+        # output.release()
+
+    def compute_trajectory_image(self, initial_frame=None):
+        fframe = initial_frame if initial_frame is not None else self.trajectory.get_first_ocurrence_frame()
+        image = self.frames[fframe]
+
+        for i in range(fframe, len(self.frames)):
+            # Draw a point at (x, y) position
+            x,y = self.trajectory.predict(i)
+            cv2.circle(image, (int(x), int(y)), 15, (0, 0, 255), -1)
+
+        return image
+    
+    def export_trajectory_image(self, initial_frame=None):
+        image = self.compute_trajectory_image(initial_frame)
+
+        # Display the frame
+        cv2.imshow('frame', image)
+        cv2.waitKey()
+
+    
+    def show_trajectory_image(self, initial_frame=None):
+        image = self.compute_trajectory_image(initial_frame)
+
+        # Display the frame
+        cv2.imshow('frame', image)
+        cv2.waitKey()
 
 
-# result = BlobDetector.video_detector(r'D:\Work\UH-IA\Computer Vision\dataset\youtube\1.mp4')
-cap = cv2.VideoCapture(r'D:\Work\UH-IA\Computer Vision\dataset\youtube\1.mp4')
-medianFrame = ImageProcessor.get_median_frame(cap)
-
-# Reset frame number to 0
-cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-
-frames = []
-while cap.isOpened():
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-
-    frames.append(frame)
-
-    # keypoints = BlobDetector.frame_detector(nframe)
-    # nframe = cv2.drawKeypoints(nframe, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    # nframe = ImageProcessor.back_substraction(frame)
-    # cv2.imshow('frame', nframe)
-
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-cap.release()
-
-fg_video = ImageProcessor.video_back_substraction(frames, medianFrame)
-result = BlobDetector.video_detector_from_frames(fg_video)
-video_blobs = BlobDetector.unpack_video_blobs(result)
-
-trajectory = Trajectory(video_blobs)
-trajectory.plot_x_distribution()
-trajectory.plot_y_distribution()
+v_trajectory = VideoTrajectory()
+v_trajectory.compute_trajectory(r'D:\Work\UH-IA\Computer Vision\dataset\videos\videos\video3.avi')
+v_trajectory.show_trajectory_image()
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,24 @@
+import cv2
 
 def keypoint_to_coordinates(kp):
     return (kp.pt[0], kp.pt[1])
+
+def load_video(path):
+    cap = cv2.VideoCapture(path)
+
+    # Reset frame number to 0
+    cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+    frames = []
+    while cap.isOpened():
+        ret, frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        frames.append(frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    return cap, frames
